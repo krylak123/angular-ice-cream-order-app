e525a6bdabf37c025808ebc294e3f2b0028ff3a9
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-05-01 18:00:02 +0200

    feat: add login panel

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 import { AuthComponent } from './auth.component';
 
@@ -8,6 +12,10 @@ import { AuthComponent } from './auth.component';
   declarations: [AuthComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
     RouterModule.forChild([
       {
         path: '',
