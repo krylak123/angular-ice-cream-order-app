a6300edab242c275d17f4d0aa073cfd1c7f61a56
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-05-01 10:25:45 +0200

    feat: install NGRX DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,30 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '@environments/environment';
+
+import { AppState } from './store/app.state';
 import { authReducer } from './store/auth';
 
 import { AppComponent } from './app.component';
-import { AppState } from './store/app.state';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot<AppState>({
       auth: authReducer,
     }),
-    HttpClientModule,
-    CommonModule,
-    BrowserAnimationsModule,
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production,
+      autoPause: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
