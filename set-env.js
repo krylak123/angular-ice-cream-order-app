e062422432a69d46d8d3a809b8b0e1cd39d3c65c
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-05-01 17:37:49 +0200

    feat: add databaseURL to .env

--- set-env.js
+++ set-env.js
@@ -14,6 +14,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: "${process.env.API_KEY}",
     authDomain: "${process.env.AUTH_DOMAIN}",
+    databaseURL: "${process.env.DATABASE_URL}",
     projectId: "${process.env.PROJECT_ID}",
     storageBucket: "${process.env.STORAGE_BUCKET}",
     messagingSenderId: "${process.env.MESSAGING_SENDER_ID}",
