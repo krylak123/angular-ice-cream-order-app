116eda8d6169707141a1c8108e396e3ebeebdeda
Krystian Grzelak <krystian.grzelak@envelo.pl>
2022-05-05 11:09:43 +0200

    feat: change types of store states
    Added type any of data in product.stats.ts
    Added type any of data in units.stats.ts

--- src/app/store/units/units.state.ts
+++ src/app/store/units/units.state.ts
@@ -1,4 +1,5 @@
 export interface UnitsState {
   key: string | null;
-  data: UnitsState | null;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: UnitsState | null | any;
 }
