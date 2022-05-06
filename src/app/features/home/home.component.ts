9dd6955f2e7186a94a26a111e3387986d6033d12
Krystian Grzelak <krystian.grzelak@envelo.pl>
2022-05-06 10:40:49 +0200

    feat: create structure of home page component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public user$ = this.store.select(store => store.user);
+  public isTest = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private store: Store<AppState>) {}
 }
