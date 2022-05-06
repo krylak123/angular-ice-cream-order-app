7f65582dfecba1b26a0a8ebdb1d0de2e7ce022fa
Krystian Grzelak <krystian.grzelak@envelo.pl>
2022-05-06 13:15:40 +0200

    feat: add toast notifications for new order

--- src/app/features/new-order/new-order.service.ts
+++ src/app/features/new-order/new-order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Store } from '@ngrx/store';
+import { ToastrService } from 'ngx-toastr';
 import { of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AppState } from 'src/app/store/app.state';
@@ -23,7 +24,11 @@ export interface OrderList {
 export class NewOrderService {
   public user$ = this.store.select(store => store.user);
 
-  constructor(private http: HttpClient, private store: Store<AppState>) {}
+  constructor(
+    private http: HttpClient,
+    private store: Store<AppState>,
+    private toastr: ToastrService
+  ) {}
 
   public checkUserOrders(userKey: string, currentOrder: Order, newOrder: Order) {
     this.setNewOrder(userKey, newOrder);
@@ -41,7 +46,9 @@ export class NewOrderService {
       .pipe(catchError(err => of(err)))
       .subscribe(res => {
         if (res instanceof HttpErrorResponse) {
-          console.error(res);
+          this.toastr.error('Wystąpił problem', 'Błąd!');
+        } else {
+          this.toastr.success('Dodano zamówienie', 'Sukces!');
         }
       });
   }
@@ -54,7 +61,7 @@ export class NewOrderService {
       .pipe(catchError(err => of(err)))
       .subscribe(res => {
         if (res instanceof HttpErrorResponse) {
-          console.error(res);
+          this.toastr.error('Wystąpił problem', 'Błąd!');
         }
       });
   }
