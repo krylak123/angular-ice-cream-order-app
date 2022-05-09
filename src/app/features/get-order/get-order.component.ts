e6106430ee2368c4430754caea2715c2a6beee00
Krystian Grzelak <krystian.grzelak@o2.pl>
2022-05-09 09:33:27 +0200

    refactor: move get orders logic to service
    Using store for get orders.

--- src/app/features/get-order/get-order.component.ts
+++ src/app/features/get-order/get-order.component.ts
@@ -1,15 +1,9 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { OrderList } from '@features/new-order/new-order.service';
 import { UsersService } from '@features/users-list/users.service';
 import { Store } from '@ngrx/store';
-import { format, parseISO } from 'date-fns';
-import { BehaviorSubject, map } from 'rxjs';
+import { format } from 'date-fns';
 import { AppState } from 'src/app/store/app.state';
-
-export interface OrderByClient {
-  email: string;
-  order: OrderList[];
-}
+import { GetOrderService } from './get-order.service';
 
 @Component({
   selector: 'app-get-order',
@@ -19,13 +13,15 @@ export interface OrderByClient {
 })
 export class GetOrderComponent implements OnInit {
   public displayedColumns: string[] = ['position', 'product', 'unit', 'count'];
-  public users$ = this.store.select(store => store.users);
-  public orders$ = new BehaviorSubject<OrderList[]>([]);
-  public ordersByClient$ = new BehaviorSubject<OrderByClient[]>([]);
+  public orders$ = this.store.select(store => store.orders);
   public toggleTable: 'ORDERS' | 'CLIENT' | '' = '';
   public todayDay: string = format(new Date(), 'dd.MM.yyyy');
 
-  constructor(private usersService: UsersService, private store: Store<AppState>) {}
+  constructor(
+    private usersService: UsersService,
+    private store: Store<AppState>,
+    private getOrdersService: GetOrderService
+  ) {}
 
   public ngOnInit(): void {
     this.usersService.getUsers();
@@ -34,41 +30,12 @@ export class GetOrderComponent implements OnInit {
   public getOrders() {
     this.toggleTable = 'ORDERS';
 
-    this.users$
-      .pipe(
-        map(users => users.map(user => user.data.currentOrder)),
-        map(orders => orders.filter(order => order !== undefined)),
-        map(orders =>
-          orders.filter(order => format(parseISO(order.date), 'dd') === format(new Date(), 'dd'))
-        ),
-        map(orders => orders.map(order => order.order))
-      )
-      .subscribe(res => {
-        const final: OrderList[] = [];
-        res.forEach(item => final.push(...item));
-        this.orders$.next(final);
-      });
+    this.getOrdersService.getOrders();
   }
 
   public getOrdersByClients() {
     this.toggleTable = 'CLIENT';
 
-    this.users$
-      .pipe(
-        map(users => users.filter(user => user.data.currentOrder !== undefined)),
-        map(users =>
-          users.filter(
-            user => format(parseISO(user.data.currentOrder.date), 'dd') === format(new Date(), 'dd')
-          )
-        ),
-        map(users =>
-          users.map(user => {
-            const { order } = user.data.currentOrder;
-
-            return { email: user.data.email, order };
-          })
-        )
-      )
-      .subscribe(res => this.ordersByClient$.next(res));
+    this.getOrdersService.getOrdersByClients();
   }
 }
